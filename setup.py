0fdfebcfc57ab3c37f9f6aa1bf1a64ec03665e08
SubhankarGhosh1233 <subhankar333ghosh@gmail.com>
2022-12-02 16:26:22 +0530

    added notebook

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="subhankar333ghosh@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements(),
+
+)
