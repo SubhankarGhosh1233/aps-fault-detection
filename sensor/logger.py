026ec0464f67d8f0fe0bc3775262c8994a822653
SubhankarGhosh1233 <subhankar333ghosh@gmail.com>
2022-12-08 20:15:14 +0530

    updated and modified

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,4 +1,4 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 import os
@@ -11,6 +11,7 @@ LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
 #create folder if not available
 os.makedirs(LOG_FILE_DIR,exist_ok=True)
+
 #log file path
 
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
